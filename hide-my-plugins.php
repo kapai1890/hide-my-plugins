0e39ebb4aa3c654ea475ed1862b0729f80b9af31
Yevhen Bilyavskyi <kapai1890@gmail.com>
2019-03-25 23:44:18 +0200

    Check environment + use real plugin dir for translations

--- hide-my-plugins.php
+++ hide-my-plugins.php
@@ -16,9 +16,21 @@ if (!defined('ABSPATH')) {
     exit('Press Enter to proceed...');
 }
 
+define('HideMyPlugins\PLUGIN_DIR', plugin_dir_path(__FILE__)); // With trailing slash
 define('HideMyPlugins\PLUGIN_URL', plugin_dir_url(__FILE__));
 
-require_once __DIR__ . '/includes/Plugin.php';
+if (version_compare(PHP_VERSION, '5.4', '>=') && version_compare(get_bloginfo('version'), '4.7', '>=')) {
+    require_once __DIR__ . '/includes/Plugin.php';
 
-global $hideMyPlugins;
-$hideMyPlugins = new HideMyPlugins\Plugin();
+    // Create instance of the plugin
+    HideMyPlugins\Plugin::getInstance();
+
+} else {
+    // Show error message
+    add_action('admin_notices', function () {
+        /* translators: %1$s: PHP version; %2$s: WordPress version */
+        $message = sprintf(esc_html__('Hide My Plugins requires PHP version %1$s+ and WordPress version %2$s+, and cannot run in the current environment.', 'hide-my-plugins'), '5.4', '4.7');
+
+        echo '<div class="error">', wpautop($message), '</div>';
+    });
+}
